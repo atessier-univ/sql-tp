94f65fcfd951a1fd4ba45e6e262840dce34df303
Anael TESSIER <anael.tessier@etu.univ-tours.fr>
2025-05-19 14:42:55 +0000

    finish ex1

--- 2025_BDD_TESSIER_ANAEL_EX1.sql
+++ 2025_BDD_TESSIER_ANAEL_EX1.sql
@@ -1,6 +1,6 @@
 -- Table personnes
 -- clef primaire pour chaque etudiant, en serial pour AUTOINCREMENT
--- nom et prenom en VARCHAR(100) car taille variable, non null car toute personnes à un nom
+-- 'nom' et 'prenom' en VARCHAR(100) car taille variable, non null car toute personnes à un nom
 -- N° étudiant, propre à chaque étudiant, non null car chaque étudiant en a un
 CREATE TABLE etudiants(
    etudiant_id SERIAL,
@@ -10,19 +10,29 @@ CREATE TABLE etudiants(
    PRIMARY KEY(etudiant_id)
 );
 
+
+-- Table du materiel
+-- Clef primaire pour chaque materiel, en serial pour AUTOINCREMENT
+-- 'nom' en VARCHAR(100) car taille variable
 CREATE TABLE materiels(
    materiel_id SERIAL,
    nom VARCHAR(100) NOT NULL UNIQUE,
    PRIMARY KEY(materiel_id)
 );
 
+
+-- Table des reservation
+-- Clef primaire reservation_id en serial pour AUTOINCREMENT
+-- FK ne peuvent pas être vide : quand on reserve, il y a obligatoirement une personne qui reserve un materiel
+-- Date de début ne peut être nulle, par défaut elle prend la date actuelle
+-- Date de fin est une date prévue et donc ne peut être nule
 CREATE TABLE reservations(
    reservation_id SERIAL,
-   date_debut DATE,
-   date_fin DATE,
-   etudiant_id INT,
-   materiel_id INT,
+   date_debut DATE NOT NULL DEFAULT CURRENT_DATE,
+   date_fin DATE NOT NULL,
+   etudiant_id INT NOT NULL,
+   materiel_id INT NOT NULL,
    PRIMARY KEY(reservation_id),
-   FOREIGN KEY(etudiant_id) REFERENCES Personnes(etudiant_id),
+   FOREIGN KEY(etudiant_id) REFERENCES etudiants(etudiant_id),
    FOREIGN KEY(materiel_id) REFERENCES materiels(materiel_id)
-);
+); 
